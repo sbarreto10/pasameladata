f7c15728fbaf706539d3e82c06d2c30fc55e2525
sbarreto10 <sbarreto@fi.uba.ar>
2023-11-15 15:21:40 -0300

    Hacer responsive plantilla estática de página de evento
    Añadir transiciones de estilo con scripts

---
+++ pages/event-page.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const picContainers = document.querySelectorAll(".pic-container");
+
+   picContainers.forEach((container) => {
+       container.addEventListener("mouseover", () => {
+           // Cuando el mouse está sobre un elemento, la opacidad de todos cambia a 0.5
+           picContainers.forEach((c) => {
+               c.classList.add("pic-container-hover");
+           });
+
+           // Excepto el que está en :hover
+           container.classList.remove("pic-container-hover");
+       });
+
+       container.addEventListener("mouseout", () => {
+           // Cuando el mouse sale de un elemento, la opacidad de todos cambia a 1
+           picContainers.forEach((c) => {
+               c.classList.remove("pic-container-hover");
+           });
+       });
+   });
+});
